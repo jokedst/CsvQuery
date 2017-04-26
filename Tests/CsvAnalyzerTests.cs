9c72e1bbe5c0f2d07e33403bca738df7e4c37b2b
jokedst <jokedst@gmail.com>
2017-04-26 20:54:55 +0200

    testing files

--- Tests/CsvAnalyzerTests.cs
+++ Tests/CsvAnalyzerTests.cs
@@ -1,5 +1,6 @@
 ﻿namespace Tests
 {
+    using System.IO;
     using CsvQuery.Csv;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -22,9 +23,17 @@
         [TestMethod]
         public void CanFindSemicolonInBigFile()
         {
-            var csvData = Helpers.GetResource("Avtalsprislista992220_20140623-112840802.csv");
+            var csvData = Helpers.GetResource("semicolon.csv");
             var result = CsvAnalyzer.Analyze(csvData);
             Assert.AreEqual(';', result.Separator);
+        }
+
+        [TestMethod]
+        public void CanReadFiles()
+        {
+            var csvData = File.ReadAllText(@"TestFiles\sentences.csv");
+            var result = CsvAnalyzer.Analyze(csvData);
+            Assert.AreEqual(',', result.Separator);
         }
     }
 }
--- Tests/Helpers.cs
+++ Tests/Helpers.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Tests
+﻿namespace Tests
 {
     using System.IO;
     using System.Reflection;
@@ -13,7 +8,6 @@ namespace Tests
         public static string GetResource(string resourceName)
         {
             var assembly = Assembly.GetExecutingAssembly();
-            // var resourceName = "MyCompany.MyProduct.MyFile.txt";
 
             using (Stream stream = assembly.GetManifestResourceStream("Tests.TestFiles." + resourceName))
             using (var reader = new StreamReader(stream))
